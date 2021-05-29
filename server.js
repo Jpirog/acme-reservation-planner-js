96a3dc4422295ba980dcc3b739c80eb63e589758
John Pirog <jpirog@hotmail.com>
2021-05-29 18:31:15 -0500

    inor change?

--- server.js
+++ server.js
@@ -132,7 +132,7 @@ const express = require('express');
 const app = express();
 const chalk = require('chalk');
 const path = require('path');
-app.use(express.json());
+//app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
